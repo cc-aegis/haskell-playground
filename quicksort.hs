28d653461c932e87acfb697a3bd6dc351882f31c
Louis Goerlt <louis.goerlt@gmail.com>
2026-08-23 10:01:55 +0200

    use Data.List.partition for quicksort

--- quicksort.hs
+++ quicksort.hs
@@ -1,14 +1,9 @@
-subdivide :: (a -> Bool) -> [a] -> ([a], [a])
-subdivide _ [] = ([], [])
-subdivide cond (x:xs)
-    | cond x = (x:lhs, rhs)
-    | otherwise = (lhs, x:rhs)
-        where (lhs, rhs) = subdivide cond xs
+import Data.List (partition)
 
 quicksort :: Ord a => [a] -> [a]
 quicksort [] = []
 quicksort (x:xs) = (quicksort lhs) ++ [x] ++ (quicksort rhs)
-    where (lhs, rhs) = subdivide (\e -> e < x) xs
+    where (lhs, rhs) = partition (\e -> e < x) xs
 
 main :: IO ()
 main = do
