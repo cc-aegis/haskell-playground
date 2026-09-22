1dcc3783a2c0d90ab11969aa05814e0ce7231ccf
Louis Goerlt <louis.goerlt@gmail.com>
2026-09-22 10:06:21 +0200

    hell yeah i did it again

--- fold.hs
+++ fold.hs
@@ -6,11 +6,23 @@ foldr' :: (a -> b -> b) -> b -> [a] -> b
 foldr' _ base [] = base
 foldr' combine base (x:xs) = combine x (foldr' combine base xs)
 
+
+foldl'' :: (b -> a -> b) -> b -> [a] -> b
+foldl'' _ i [] = i
+foldl'' f i (x:xs) = foldl'' f (f i x) xs
+
+foldr'' :: (a -> b -> b) -> b -> [a] -> b
+foldr'' _ i [] = i
+foldr'' f i (x:xs) = f x (foldr'' f i xs)
+
+
 main :: IO ()
 main = do
     let nums = ["1", "2", "3"]
     let join a b = "(" ++ a ++ ", " ++ b ++ ")"
     print $ foldl join "b" nums
     print $ foldl' join "b" nums
+    print $ foldl'' join "b" nums
     print $ foldr join "b" nums
     print $ foldr' join "b" nums
+    print $ foldr'' join "b" nums
